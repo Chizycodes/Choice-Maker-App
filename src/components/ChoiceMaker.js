389ff943dc1944ec87ac6c218fbdee2375807ed2
Joy Okwudire <okwudirejoy@gmail.com>
2021-11-01 08:52:33 +0100

    add option field

--- src/components/ChoiceMaker.js
+++ src/components/ChoiceMaker.js
@@ -7,8 +7,10 @@ const ChoiceMaker = () => {
   const [options, setOptions] = useState([
     { option1: "", option2: "", option3: "" },
   ]);
+  const [moreOptions, setMoreOptions] = useState([{
+    option4: ""
+  }])
   const [randomAnswer, setRandomAnswer] = useState("");
-
 
   useEffect(() => {
     localStorage.setItem("randomAnswer", randomAnswer);
@@ -16,34 +18,49 @@ const ChoiceMaker = () => {
   }, [randomAnswer, question]);
 
   const handleChange = (e) => {
+    const { name, value } = e.target;
     const values = [...options];
-    values[0][e.target.name] = e.target.value;
+    values[0][name] = value;
     setOptions(values);
-    
   };
 
-
-const getRandomAnswer = (e) => {
-  e.preventDefault();
-  let optionsArray = Object.values(options[0]);
-  let realOptions = optionsArray.filter((item) => item !== "");
-
-  const random = realOptions[Math.floor(Math.random() * realOptions.length)];
-
-  setRandomAnswer(random);
-  window.location = `${window.location}displaychoice`;
-};
-  
+  const handleMoreChanges = (e, index) => {
+    const {name, value } = e.target;
+    const list = [...moreOptions];
+    list[index][name] = value;
+    setMoreOptions(list);
+  }
 
   const addTextField = () => {
+    setMoreOptions([...moreOptions, { option4: "" }]);
   };
+
+  const getRandomAnswer = (e) => {
+    e.preventDefault();
+    
+    let optionsArray = Object.values(options[0]);
+    let moreOptionsArray = Object.values(moreOptions[0]);
+    let combinedArray = [...optionsArray, ...moreOptionsArray];
+
+    console.log(combinedArray);
+
+    let realOptions = combinedArray.filter((item) => item !== "");
+
+    const random = realOptions[Math.floor(Math.random() * realOptions.length)];
+
+    setRandomAnswer(random);
+    window.location = `${window.location}displaychoice`;
+  };
+
+  
 
   return (
     <>
-
       <div className="question-div">
-        <h2>Enter Your Question<span>*</span></h2>
-  
+        <h2>
+          Enter Your Question<span>*</span>
+        </h2>
+
         <input
           type="text"
           required
@@ -56,7 +73,7 @@ const getRandomAnswer = (e) => {
 
       <div className="answers-div">
         <h2>What are your options?</h2>
-          <h4>Enter atleast 2 options*</h4>
+        <h4>Enter atleast 2 options*</h4>
         <TextField
           required
           id={1}
@@ -77,16 +94,30 @@ const getRandomAnswer = (e) => {
           handleChange={handleChange}
           inputName="option3"
         />
+        {moreOptions.map((x, i) => {
+          return (
+            <TextField
+              id={i+4}
+              key={i}
+              inputValue={x.option4}
+              handleChange={(e) => handleMoreChanges(e, i)}
+              inputName="option4"
+            />
+          );
+        })}
       </div>
 
       <div className="buttons">
-        <Button color="grey" text="+ Option" onClick={addTextField} />
+        <Button color="grey" text="+ Option" onClick={() => addTextField()} />
 
         <Button
           color="rgb(65, 146, 27)"
           text="CHOOSE"
           onClick={getRandomAnswer}
-          disabled={!question || Object.values(options[0]).filter((item) => item !== "").length < 2}
+          disabled={
+            !question ||
+            Object.values(options[0]).filter((item) => item !== "").length < 2
+          }
         />
       </div>
     </>
